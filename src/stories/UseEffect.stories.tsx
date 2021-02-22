ffe72ca752f2335969e52494acdf4374d9cf1e95
zhankaam <zhankaam@gmail.com>
2021-02-22 10:10:15 +0300

    kkp cloud

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -40,13 +40,13 @@ export const SimpleExample = () => {
     </>
 }
 
-export const SetTimeOutExample = () => {
+export const SetIntervalExample = () => {
     const [fake, setFake] =useState(1)
     const [counter, setCounter] =useState(1)
     console.log("SetTimeOutExample")
 
     useEffect(() => {
-        setInterval( ()=> {
+       const setIntervalId = setInterval( ()=> {
             setCounter(state => state + 1)
         }, 1000)
 /*
@@ -54,6 +54,9 @@ export const SetTimeOutExample = () => {
             console.log('setTimeOut')
             document.title = counter.toString();
         }, 1000)*/
+       return ()  => {
+           clearInterval(setIntervalId)
+        }
 
     },[])
 
@@ -91,11 +94,36 @@ export const KeysTrackerExample = () => {
     console.log("Component rendered " + text)
 
     useEffect(() => {
-       window.addEventListener("keypress", (e) => {
-           console.log(e.key)
-           setText((state) => state + e.key)
-       })
+        const handler = (e: KeyboardEvent) => {
+            console.log(e.key)
+            setText((state) => state + e.key)
+        }
 
+       window.addEventListener("keypress",handler )
+       return () => {
+            window.removeEventListener('keypress', handler)
+       }
+    },[text])
+
+
+    return <>
+        Typed text: {text}
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    const [text, setText] =useState("")
+
+    console.log("Component rendered " + text)
+
+    useEffect(() => {
+
+       const TimeoutId = setTimeout(() => {
+           setText('3 seconds passed')
+       },3000)
+        return () => {
+             clearInterval(TimeoutId)
+        }
     },[text])
 
 
